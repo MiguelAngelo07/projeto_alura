d79dbd4334be8895d88198a79bb9a33176b0d669
MiguelAngelo07 <miguelsouza161220@gmail.com>
2024-09-05 23:30:57 -0400

    alterações na base de dados.js

--- app.js
+++ app.js
@@ -1,1 +1,1 @@
-console.log("Atletas BR");
+console.log(dados);
---
+++ dados.js
@@ -0,0 +1,20 @@
+let dados = [
+    {
+        titulo : "Rayssa Leal",
+        descricao : "Rayssa Leal, conhecida como 'Fadinha', é uma skatista brasileira que conquistou o mundo com seu talento e carisma. Aos 13 anos, tornou-se a mais jovem medalhista olímpica da história do Brasil.",
+        link : "https://pt.wikipedia.org/wiki/Rayssa_Leal",
+        esporte : "Skate"
+    },
+    {
+        titulo : "Beatriz Souza",
+        descricao : "Beatriz Souza é uma judoca brasileira que se destaca pela sua técnica e determinação. Com diversas medalhas em competições nacionais e internacionais, ela é uma das principais promessas do judô brasileiro.",
+        link : "https://pt.wikipedia.org/wiki/Beatriz_Souza",
+        esporte : "Judô"
+    },
+    {
+        titulo : "Rebeca Andrade",
+        descricao : "Ginasta artística brasileira, bicampeã olímpica, conhecida por sua versatilidade e determinação. É um dos maiores nomes do esporte brasileiro.",
+        link : "https://pt.wikipedia.org/wiki/Rebeca_Andrade",
+        esporte : "Ginástica Artística"
+    }
+];
