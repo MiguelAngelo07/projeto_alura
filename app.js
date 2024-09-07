475a589228217e438092e12f00cee19cbc61fc36
MiguelAngelo07 <miguelsouza161220@gmail.com>
2024-09-07 15:46:00 -0400

    dando continuidade no projeto

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
-console.log(dados);
+// console.log(dados);
+// comentário
+let section = document.getElementById("resultados-pesquisa")
+console.log(section);
+
+let resultados = ""
+
+for(let dado of dados) {
+    resultados += `
+    <div class="item-resultado">
+        <h2>
+            <a href=${dado.social} target="_blank">${dado.titulo}</a>
+        </h2>
+        <p class="descricao-meta">${dado.descricao}</p>
+        <a href=${dado.link} target="_blank">Mais informações</a>
+    </div>;`}
+
--- dados.js
+++ dados.js
@@ -3,18 +3,21 @@ let dados = [
         titulo : "Rayssa Leal",
         descricao : "Rayssa Leal, conhecida como 'Fadinha', é uma skatista brasileira que conquistou o mundo com seu talento e carisma. Aos 13 anos, tornou-se a mais jovem medalhista olímpica da história do Brasil.",
         link : "https://pt.wikipedia.org/wiki/Rayssa_Leal",
-        esporte : "Skate"
+        esporte : "Skate",
+        social : "https://www.instagram.com/rayssalealsk8/?hl=pt-br"
     },
     {
         titulo : "Beatriz Souza",
         descricao : "Beatriz Souza é uma judoca brasileira que se destaca pela sua técnica e determinação. Com diversas medalhas em competições nacionais e internacionais, ela é uma das principais promessas do judô brasileiro.",
         link : "https://pt.wikipedia.org/wiki/Beatriz_Souza",
-        esporte : "Judô"
+        esporte : "Judô",
+        social : "https://www.instagram.com/_beatrizsouzaar/"
     },
     {
         titulo : "Rebeca Andrade",
         descricao : "Ginasta artística brasileira, bicampeã olímpica, conhecida por sua versatilidade e determinação. É um dos maiores nomes do esporte brasileiro.",
         link : "https://pt.wikipedia.org/wiki/Rebeca_Andrade",
-        esporte : "Ginástica Artística"
+        esporte : "Ginástica Artística",
+        social : "https://www.instagram.com/rebecarandrade/?hl=pt-br"
     }
 ];
