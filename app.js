51787453ab674f920568a939cbe4959d0af4317c
MiguelAngelo07 <miguelsouza161220@gmail.com>
2024-09-07 17:17:36 -0400

    falta corrigir o botão pesquisar

--- app.js
+++ app.js
@@ -1,20 +1,26 @@
 function pesquisar(){
     
 
-let section = document.getElementById("resultados-pesquisa")
-console.log(section);
+let section = document.getElementById("resultados-pesquisa");
+
+let campoPesquisa = document.getElementById("campo-pesquisa").value
+
+console.log(campoPesquisa)
 
 let resultados = ""
 
 for(let dado of dados) {
-    resultados += `
+    if (dado.titulo.includes(campoPesquisa)){
+        resultados += `
     <div class="item-resultado">
         <h2>
             <a href=${dado.social} target="_blank">${dado.titulo}</a>
         </h2>
         <p class="descricao-meta">${dado.descricao}</p>
         <a href=${dado.link} target="_blank">Mais informações</a>
-    </div>;`}
+    </div>`;
+    }
+}
 
-    section.innerHTML = resultados
+section.innerHTML = resultados    
 }
